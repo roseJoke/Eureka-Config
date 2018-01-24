527dd7c9ee04d86d7aede655c333b99d3f64ed67
roseJoke <1136859952@qq.com>
2018-01-24 10:01:17 +0800

    config  -server config   change

--- e-ithou/config-client/src/main/java/com/kuliao/configclient/controller/TestController.java
+++ e-ithou/config-client/src/main/java/com/kuliao/configclient/controller/TestController.java
@@ -1,25 +1,29 @@
 package com.kuliao.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
 public class TestController {
-   /*@Value("${from}")
+  /* @Value("${from}")
     private String from;
 
-   @RequestMapping("/from")
-   public String from(){
-       return this.from;
-   }*/
+    @RequestMapping("/from")
+    public String from(){
+        return this.from;
+    }*/
    @Autowired
    private Environment env;
 
+   @RequestMapping("/from")
    public String from(){
-       return env.getProperty("from","undefined");
+       return env.getProperty("from");
    }
 }
